46d89343e3386dcc293dc05ece1763ece246ccc1
Nathan Braswell <miloignis@gmail.com>
2019-03-09 01:26:14 -0500

    Scrollback, message sending, display images (gifs frozen)

--- bubbles/App.js
+++ bubbles/App.js
@@ -1,12 +1,44 @@
 import React from 'react'
-import { Button, Text, View, TextInput, FlatList, TouchableWithoutFeedback } from 'react-native';
+import { Button, Text, View, TextInput, FlatList, TouchableWithoutFeedback, YellowBox } from 'react-native';
 import { GiftedChat } from 'react-native-gifted-chat'
 import sdk from 'matrix-js-sdk'
 
+YellowBox.ignoreWarnings(['Setting a timer'])
+
 let client = null
 
+function messageEventToGiftedChatMessage(room, event) {
+  let user = room.getMember(event.getSender());
+  let message = {
+      _id: event.getId(),
+      text: event.getContent().body,
+      createdAt: event.getDate(),
+      user: {
+          _id: user.userId,
+          name: user.name,
+          avatar: user.getAvatarUrl(client.getHomeserverUrl(),
+              100,
+              100,
+              "scale",
+              true,
+              true),
+      }
+  };
+ if (event.getContent().msgtype == "m.image") {
+     message.image = client.mxcUrlToHttp(event.getContent().url);
+ }
+ if (event.getContent().msgtype == "m.video") {
+     message.video = client.mxcUrlToHttp(event.getContent().url);
+ }
+  return message;
+}
 export class ChatScreen extends React.Component {
+  static navigationOptions = {
+      title: 'Chat',
+  };
   state = {
+    roomId: null,
+    room: null,
     messages: [],
   }
 
@@ -16,45 +48,43 @@ export class ChatScreen extends React.Component {
     let messages = room.getLiveTimeline().getEvents()
                        .filter(event => event.getType() == "m.room.message")
                        .reverse()
-                       .map(event => {
-                           let user = room.getMember(event.getSender());
-                           return {
-                                _id: event.getId(),
-                                text: event.getContent().body,
-                                createdAt: event.getDate(),
-                                user: {
-                                    _id: user.userId,
-                                    name: user.name,
-                                    avatar: user.getAvatarUrl(client.getHomeserverUrl(),
-                                                              100,
-                                                              100,
-                                                              "scale",
-                                                              true,
-                                                              true),
-                                }
-                           };
-                       });
+                       .map((event) => messageEventToGiftedChatMessage(room, event));
 
     this.setState({
+      roomId: roomId,
+      room: room,
       messages: messages
     });
-
+    client.on("Room.timeline", (event, eRoom, toStartOfTimeline) => {
+        if (eRoom == room && event.getType() == "m.room.message") {
+            let message = messageEventToGiftedChatMessage(room, event)
+            this.setState(previousState => ({
+                messages: toStartOfTimeline ? previousState.messages.concat([message])
+                                            : [message].concat(previousState.messages),
+            }))
+        }
+    })
+    // we want more scrollback to start with
+      client.scrollback(room, 30, () => {})
   }
 
   onSend(messages = []) {
-    this.setState(previousState => ({
-      messages: GiftedChat.append(previousState.messages, messages),
-    }))
+    for (let message of messages) {
+        client.sendEvent(this.state.roomId, "m.room.message",
+                         { "body": message.text, "msgtype": "m.text" }, "")
+    }
   }
 
   render() {
-    //const {navigate} = this.props.navigation;
     return (
       <GiftedChat
         messages={this.state.messages}
         onSend={messages => this.onSend(messages)}
+        loadEarlier={ true }
+        onLoadEarlier={ () => client.scrollback(this.state.room, 30, () => {}) }
+        scrollToBottom = { true }
         user={{
-          _id: 1,
+          _id: client.getUserId(),
         }}
       />
     )
@@ -77,21 +107,18 @@ export class LoginScreen extends React.Component {
         return (
             <View>
                 <TextInput
-                    placeholder="server: https://room409.xyz"
                     onChangeText={(text) => {
                         this.setState((previousState) => ({server: text}));
                     }}
                     defaultValue={this.state.server}
                 />
                 <TextInput
-                    placeholder="username: miloignis"
                     onChangeText={(text) => {
                         this.setState((previousState) => ({user: text}));
                     }}
                     defaultValue={this.state.user}
                 />
                 <TextInput
-                    placeholder="password: hunter2"
                     onChangeText={(text) => {
                         this.setState((previousState) => ({password: text}));
                     }}
@@ -109,16 +136,31 @@ export class LoginScreen extends React.Component {
                                 client.startClient();
                                 client.once('sync', (state, prevState, res) => {
                                     console.log("all synced!")
+                                    navigate('Rooms', {somin: 'walla'})
                                 })
                             });
-                        navigate('Rooms', {somin: 'walla'})
                     } }
                 />
             </View>
         );
     }
 }
-
+function getRooms() {
+    return client.getRooms().sort((a,b) => {
+                let aEvents = a.getLiveTimeline().getEvents();
+                let aMsg = aEvents[aEvents.length-1];
+                if (!aMsg) return 1;
+                let bEvents = b.getLiveTimeline().getEvents();
+                let bMsg = bEvents[bEvents.length-1];
+                if (!bMsg) return -1;
+                if (aMsg.getTs() > bMsg.getTs()) {
+                    return -1;
+                } else if (aMsg.getTs() < bMsg.getTs()) {
+                    return 1;
+                }
+                return 0;
+            }).map(room => ({key: room.roomId, title: room.name}))
+}
 export class RoomsScreen extends React.Component {
     static navigationOptions = {
         title: 'Rooms',
@@ -130,27 +172,24 @@ export class RoomsScreen extends React.Component {
         };
     }
     componentWillMount() {
+        this.setState(previousState => ({ rooms: getRooms() }));
         client.on("Room.timeline", (event, room, toStartOfTimeline) => {
-            console.log("got event!")
-            console.log(event.event)
-            this.setState(previousState => ({
-                rooms: client.getRooms().map(room => ({key: room.roomId, title: room.name}))
-            }));
+            this.setState(previousState => ({ rooms: getRooms() }));
         })
     }
     render() {
         const {navigate} = this.props.navigation;
         return (
             <View>
-                <Text>Room list!</Text>
                 <FlatList
                     data={this.state.rooms}
                     renderItem={({item}) =>
-                        <TouchableWithoutFeedback
-                            onPress={ () => navigate('Chat', {roomId: item.key}) }>
-                        
-                            <Text>{item.key + "/" + item.title}</Text>
-                        </TouchableWithoutFeedback>
+                        <View style={ {padding: 10}}>
+                            <TouchableWithoutFeedback
+                                onPress={ () => navigate('Chat', {roomId: item.key}) }>
+                                <Text>{item.title}</Text>
+                            </TouchableWithoutFeedback>
+                        </View>
                     }
                 />
             </View>
